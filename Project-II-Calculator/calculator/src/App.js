cef403026702e92e0d36da57889e2b339e1f4d91
larrysimiyu <larrysimiyu@Larrys-MacBook-Pro.local>
2019-02-12 14:22:39 -0800

    components done

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
+
 
 const App = () => {
   return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+        <div className='main-Container'>
+          <CalculatorDisplay />
+          <ActionButton actionStyle='act-style' actionText='clear'/>
+          <NumberButton buttonStyle='act-style' text='/'/>
+          <NumberButton buttonStyle='btn-Color' text='7'/>
+          <NumberButton buttonStyle='btn-Color' text='8'/>
+          <NumberButton buttonStyle='btn-Color' text='9'/>
+          <NumberButton buttonStyle='act-style' text='x'/>
+          <NumberButton buttonStyle='btn-Color' text='4'/>
+          <NumberButton buttonStyle='btn-Color' text='5'/>
+          <NumberButton buttonStyle='btn-Color' text='6'/>
+          <NumberButton buttonStyle='act-style' text='-'/>
+          <NumberButton buttonStyle='btn-Color' text='1'/>
+          <NumberButton buttonStyle='btn-Color' text='2'/>
+          <NumberButton buttonStyle='btn-Color' text='3'/>
+          <NumberButton buttonStyle='act-style' text='+'/>
+          <ActionButton  actionStyle='act-style' actionText='0'/>
+          <NumberButton buttonStyle='act-style' text='='/>
+       </div>
     </div>
+
+    
   );
 };
 
